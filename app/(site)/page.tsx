787c485ec8f205551e2062d5e9ebd2bf2b6ad469
Paul Uchenna <pauluchenna20@gmail.com>
2024-06-20 03:31:17 +0100

    Logo changed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <div className="flex min-h-full flex-col justify py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm-w-full sm:max-w-md">
         <Image
-          src="/images/viber_logo.png"
+          src="/images/logo.png"
           alt="logo"
-          width={40}
-          height={49}
+          width={190}
+          height={110}
           className="mx-auto w-auto"
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
