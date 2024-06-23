02cf7b1ca6c763c5ec3fb8a02a62a153eb2a25e3
Paul Uchenna <pauluchenna20@gmail.com>
2024-06-23 03:38:49 +0100

    Responsive main page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,9 +3,10 @@ import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <div className="flex h-full">
-      <div className="flex flex-1 flex-col justify-center items-center bg-gray-100">
-        <div className="relative w-full h-[160px]">
+    <div className="flex flex-col lg:flex-row h-full min-h-screen">
+      {/* Left section */}
+      <div className="flex flex-1 flex-col justify-center items-center bg-gray-100 p-4 lg:p-8">
+        <div className="relative w-full h-[160px] lg:h-[200px]">
           <Image
             src="/images/logo.png"
             alt="logo"
@@ -14,13 +15,15 @@ export default function Home() {
             className="w-auto h-auto"
           />
         </div>
-        <div className="w-full pl-10 py-5 text-start">
-          <p className="font-semibold">
+        <div className="w-full pt-5 text-center lg:text-start">
+          <p className="font-semibold text-lg lg:text-xl">
             Connect with friends and stay updated. Sign in or create an account
             to join us.
           </p>
         </div>
       </div>
+
+      {/* Right section */}
       <div className="flex flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
