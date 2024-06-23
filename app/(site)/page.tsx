bf8d31d9adba939c51538af1f9a32654e001257e
Paul Uchenna <pauluchenna20@gmail.com>
2024-06-23 03:24:41 +0100

    Style main page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,21 +3,32 @@ import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <div className="flex min-h-full flex-col justify py-12 sm:px-6 lg:px-8 bg-gray-100">
-      <div className="sm:mx-auto sm-w-full sm:max-w-md">
-        <Image
-          src="/images/logo.png"
-          alt="logo"
-          width={190}
-          height={110}
-          className="mx-auto w-auto"
-        />
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
+    <div className="flex h-full">
+      <div className="flex flex-1 flex-col justify-center items-center bg-gray-100">
+        <div className="relative w-full h-[160px]">
+          <Image
+            src="/images/logo.png"
+            alt="logo"
+            layout="fill"
+            objectFit="contain"
+            className="w-auto h-auto"
+          />
+        </div>
+        <div className="w-full pl-10 py-5 text-start">
+          <p className="font-semibold">
+            Connect with friends and stay updated. Sign in or create an account
+            to join us.
+          </p>
+        </div>
       </div>
-      {/*AuthForm*/}
-      <AuthForm />
+      <div className="flex flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <AuthForm />
+      </div>
     </div>
   );
 }
