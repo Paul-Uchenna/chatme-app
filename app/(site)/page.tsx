9ce6ec08539a60988d9127588001af588e812ce4
Paul Uchenna <pauluchenna20@gmail.com>
2024-06-23 03:40:30 +0100

    Responsive main page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       </div>
 
       {/* Right section */}
-      <div className="flex flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="flex flex-1 flex-col justify-center py-12 sm:px-6 lg:px-6 bg-gray-100">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             Sign in to your account
