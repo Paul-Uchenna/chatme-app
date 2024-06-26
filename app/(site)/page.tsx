82278763c889cd7d0474567c0f5f402574719abe
Paul Uchenna <pauluchenna20@gmail.com>
2024-06-26 12:19:58 +0100

    Home page fix

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -15,8 +15,8 @@ function Home() {
             className="w-auto h-auto"
           />
         </div>
-        <div className="w-full pt-5 text-center lg:text-start">
-          <p className="font-semibold text-lg lg:text-xl">
+        <div className="w-full pt-0 lg:pt-5 text-center lg:text-start">
+          <p className="font-semibold text-sm lg:text-xl">
             Connect with friends and stay updated. Sign in or create an account
             to join us.
           </p>
@@ -24,7 +24,7 @@ function Home() {
       </div>
 
       {/* Right section */}
-      <div className="flex flex-1 flex-col justify-center py-12 sm:px-6 lg:px-6 bg-gray-100">
+      <div className="flex flex-1 flex-col justify-center pt-0 pb-5 sm:px-6 lg:px-6 bg-gray-100">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             Sign in to your account
